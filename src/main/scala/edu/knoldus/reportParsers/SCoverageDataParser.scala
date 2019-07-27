b93de4b9822ffcf54d5bb69f220ffbeb94d318d8
randhir1910 <randhir.kumar@knoldus.in>
2019-07-27 12:04:07 +0530

    updated Scoverage data parser

--- src/main/scala/edu/knoldus/reportParsers/SCoverageDataParser.scala
+++ src/main/scala/edu/knoldus/reportParsers/SCoverageDataParser.scala
@@ -4,30 +4,33 @@ import akka.actor.{Actor, Props}
 import edu.knoldus.model.{ClassWiseCoverage, PackageWiseCoverage}
 import edu.knoldus.reportParsers.ParserCalling.Parse
 import edu.knoldus.util.Constant._
-import scala.xml.Elem
 import edu.knoldus.util.LoggerUtil._
+
+import scala.xml.Elem
 
 class SCoverageDataParser extends Actor {
 
   override def receive: PartialFunction[Any, Unit] = {
-    case Parse(sCoverageXml: Elem,reportType: String) =>
+    case Parse(sCoverageXml: Elem, reportType: String) =>
       if (reportType.equals(sCoverage_Report)) {
-        logger.info("File passed for SCoverage Data parser")
-        val sCoverageData = (sCoverageXml \\ PACKAGE).map { packageNode =>
-          val packageName = (packageNode \ NAME_TAG).text
-          val packageCoverage = (packageNode \\ STATEMENT_RATE).toList.headOption.map(_.toString.toDouble)
-          val classesInPackage = packageNode \\ CLASS
-
-          val classWithCoverage = classesInPackage.map { clazz =>
-            val className = (clazz \ NAME_TAG).text
-            val classCoverage = (classesInPackage \\ STATEMENT_RATE).toList.headOption.map(_.toString.toDouble)
-            classCoverage.map(coverage => ClassWiseCoverage(className, coverage))
-          }.toList.flatten
-
-          logger.info(s"classWithCoverage  : $classWithCoverage")
-          packageCoverage.map(coverage => PackageWiseCoverage(packageName, coverage, classWithCoverage))
-        }.toList.flatten
-        logger.info(s"SCoverage Data: $sCoverageData")
+        if (reportType.equals(sCoverage_Report)) {
+          logger.info("File passed for SCoverage Data parser")
+          val sCoverageData = (sCoverageXml \\ "package").map { packageNode =>
+            val packageName = (packageNode \ "@name").text
+            val packageCoverage = (packageNode \ STATEMENT_RATE).toString().toDouble
+            val classWithCoverage = (packageNode \\ "class").map { classNode =>
+              val className = (classNode \ "@name").text
+              val classCoverage = (classNode \ STATEMENT_RATE).toString().toDouble
+              ClassWiseCoverage(className, classCoverage)
+            }
+            PackageWiseCoverage(packageName, packageCoverage, classWithCoverage)
+          }
+          logger.info(s"SCoverage Data: $sCoverageData")
+        }
+        else {
+          logger.info("File failed for SCoverage Data parser")
+          sender ! None
+        }
       }
       else {
         logger.info("File failed for SCoverage Data parser")
